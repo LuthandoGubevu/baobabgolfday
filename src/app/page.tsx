383e05cf5b64f197736a4d0dc1a75680edc30f53
Keegan Baobabb <keeganbaobabb@gmail.com>
2026-01-08 10:38:57 +0000

    remove the event information section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 
 import { HeroSection } from '@/components/sections/hero-section';
 import { AboutSection } from '@/components/sections/about-section';
-import { EventInfoSection } from '@/components/sections/event-info-section';
 import { CompetitionSection } from '@/components/sections/competition-section';
 import { BookingSection } from '@/components/sections/booking-section';
 import { Separator } from '@/components/ui/separator';
@@ -13,8 +12,6 @@ export default function Home() {
       <HeroSection />
       <Separator className="bg-secondary h-px md:h-0.5" />
       <AboutSection />
-      <Separator className="bg-secondary h-px md:h-0.5" />
-      <EventInfoSection />
       <Separator className="bg-secondary h-px md:h-0.5" />
       <CompetitionSection />
       <Separator className="bg-secondary h-px md:h-0.5" />
